35d82c738e4d7baf1c5db65836828634520fadcc
Edison Martínez <edison.martinez@gmail.com>
2020-01-09 15:51:46 -0500

    feat: discovery balancing

--- licensing-service/src/main/kotlin/com/thoughtmechanix/licenses/Application.kt
+++ licensing-service/src/main/kotlin/com/thoughtmechanix/licenses/Application.kt
@@ -3,12 +3,21 @@ package com.thoughtmechanix.licenses
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
 @RefreshScope
 @EnableDiscoveryClient
-class Application
+class Application {
+    @LoadBalanced
+    @Bean
+    fun getRestTemplate(): RestTemplate {
+        return RestTemplate()
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
--- licensing-service/src/main/kotlin/com/thoughtmechanix/licenses/clients/OrganizationDiscoveryClient.kt
+++ licensing-service/src/main/kotlin/com/thoughtmechanix/licenses/clients/OrganizationDiscoveryClient.kt
@@ -1,15 +1,21 @@
 package com.thoughtmechanix.licenses.clients
 
 import com.thoughtmechanix.licenses.model.Organization
-import org.springframework.cloud.client.ServiceInstance
-import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
-
 @Component
+class OrganizationDiscoveryClient(var restTemplate: RestTemplate) {
+    fun getOrganization(organizationId: String): Organization? {
+        val restExchange = restTemplate.exchange(
+                "http://organizationservice/v1/organizations/{organizationId}", HttpMethod.GET,
+                null, Organization::class.java, organizationId)
+        return restExchange.body
+    }
+}
+
+/*
 class OrganizationDiscoveryClient(var discoveryClient: DiscoveryClient) {
     fun getOrganization(organizationId: String): Organization? {
         val restTemplate = RestTemplate()
@@ -22,3 +28,4 @@ class OrganizationDiscoveryClient(var discoveryClient: DiscoveryClient) {
         return restExchange.body
     }
 }
+        */
